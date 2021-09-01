39958698f26c1fe8b7d98232d119c94501fa7286
Rifki Ardiansyah <rifkiardiansyah670@gmail.com>
2021-09-01 07:24:38 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/bokshaul/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bokshaul
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: new SafeArea(
+          child: Container(
+            child: WebView(
+              initialUrl: 'https://forwarder.boksman.co.id',
+              javascriptMode: JavascriptMode.unrestricted,
+              gestureNavigationEnabled: true,
+            ),
+          ),
+        )
+      ),
+    );
+  }
+}
